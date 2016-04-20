13f3293513258cf9d282ee50148497457f0e66c8
Kari-Hanna <kje055@post.uit.no>
2016-04-20 12:01:29 +0200

    initual commit

--- Oblig4vaar16/src/Oppgave1/Oppgave1.java
+++ Oblig4vaar16/src/Oppgave1/Oppgave1.java
@@ -7,54 +7,25 @@ public class Oppgave1 {
 	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		String normal, reversere = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Skriv inn en setning som skal vendes om.");
-		normal = input.nextLine();
-		int length = normal.length();
-		for (int i = length - 1; i >= 0; i--)
-			reversere = reversere + normal.charAt(i);
-		System.out.println("Setningen revesert er = " + reversere);
+		String normal = input.nextLine();
+		snuTekst(normal);
 	}
 
-	public static void baklengs(String tekst) {
-		baklengs(tekst);// (tekst.length() - 1);
+	public static void snuTekst(String tekst) {
+		snuTekst(tekst, tekst.length() - 1);
 	}
 
 	public static void snuTekst(String tekst, int index) {
 		if (index >= 0) {
 			System.out.print(tekst.charAt(index));
 			count++;
-			baklengs(tekst);
+			snuTekst(tekst, index-1);
 		} else {
 			System.out.printf("\nDet er totalt %d i tekststrengen.\n", count);
 			count = 0;
 		}
-	}
-
-	// Validate string.
-	// *
-	// * @param prompt
-	// * Prompt which is shown to user
-	// * @return the string that user has provided
-	// */
-	public static String validateString(String prompt) {
-
-		String respons = "";
-
-		while (respons == null || respons.equals("")) {
-			System.out.println(prompt);
-
-			try {
-
-				respons = input.nextLine();
-
-			} catch (Exception ex) {
-
-				System.out.println("Skriv inn en gyldig respons.");
-			}
-		}
-		return respons;
 	}
 
 }
